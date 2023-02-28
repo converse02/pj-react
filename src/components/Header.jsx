27d36fe1c21bc1db39996edd6ea0cc5bd6dcd45c
converse02 <kvasyuk.serzh@bk.ru>
2023-02-28 11:06:07 +0300

    using hook <useLocation> for hide search component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import logoSvg from '../assets/img/pizza-logo.svg';
 import { selectCart } from '../redux/slices/cartSlice';
@@ -8,6 +8,8 @@ import Search from './Search';
 
 function Header() {
   const { items, totalPrice } = useSelector(selectCart);
+
+  const location = useLocation();
 
   const cartItems = items.reduce((acc, item) => (acc += item.count), 0);
 
@@ -23,7 +25,7 @@ function Header() {
             </div>
           </div>
         </Link>
-        <Search />
+        {location.pathname === '/' ? <Search /> : '  '}
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
             <span>{totalPrice} ₽</span>
